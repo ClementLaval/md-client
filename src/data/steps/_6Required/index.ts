ed10257b57e706f1f8555d89ef81329dae0555b1
ClementLaval <laval.clem.pro@gmail.com>
2024-03-16 21:09:44 +0100

    Fix error on _6Required

--- src/data/steps/_6Required/index.ts
+++ src/data/steps/_6Required/index.ts
@@ -3,15 +3,14 @@ import { Logger } from '../../../utilities/logger';
 
 export function _6Required(configs: DataConfig[]): DataConfig[] {
   // TODO: Need to retrieve all document configs to see if config are missing (like default)
-  configs.forEach(
-    (config) =>
-      config.field.required &&
+  configs.forEach((config) => {
+    config.field?.required === true &&
       !config.value &&
       Logger.error({
         name: 'REQUIRED',
         msg: `${config.field.name} data is missing but required`,
-      })
-  );
+      });
+  });
 
   return configs;
 }
